a5831b9eb3dd93e4369ea568e93a2276fc392199
Francis <francis-r-emond@hotmail.com>
2016-06-09 19:17:34 -0400

    test

---
+++ model/pokedex.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Francis on 2016-06-07.
+ */
+module.exports = {
+    pokemons: {
+        3: {
+            id: 3,
+            name: "Venusaur",
+            imageUrl: "http://cdn.bulbagarden.net/upload/thumb/a/ae/003Venusaur.png/250px-003Venusaur.png"
+        }
+    }
+};
