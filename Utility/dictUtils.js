eb550c36fc91cc9b146f14adabbb5f4d940aad23
Francis <francis-r-emond@hotmail.com>
2016-06-09 21:05:08 -0400

    dssad

--- Utility/dictUtils.js
+++ Utility/dictUtils.js
@@ -1,10 +1,9 @@
 module.exports = {
     randomChoice: function (dict) {
-        var result;
-        var count = 0;
-        for (var prop in dict)
-            if (Math.random() < 1/++count)
-                return dict[prop];
-        return null;
+        var attrs = [];
+        for (var attr in dict)
+            attrs.push(attr);
+        var r = Math.random();
+        return dict[attrs[Math.floor(r*attrs.length)]];
     }
 };
