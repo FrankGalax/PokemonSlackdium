6ef1a0198159f8bdad4ca1387f3a0c4f00172e83
Francis <francis-r-emond@hotmail.com>
2016-06-10 05:51:07 -0400

    sdad

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ function formatUptime(uptime) {
 
 var app = express();
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('App listening on port ' + port);
 });
